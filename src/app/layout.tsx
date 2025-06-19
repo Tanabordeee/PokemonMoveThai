66d6814ba1fd1252dada31b4d006742dc6ae6a2d
Tanabordee Pateepbutsagon <102315593+Tanabordeee@users.noreply.github.com>
2025-06-19 09:34:10 +0700

    chore : add Analytics and Structured Data

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Kanit } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next"
 import "./globals.css";
+import AdSense from "./Adsense";
 
 const kanit = Kanit({
   subsets: ['thai', 'latin'], // รองรับภาษาไทย
@@ -12,6 +14,8 @@ const kanit = Kanit({
 export const metadata: Metadata = {
   title: "POKEMON MOVE THAI",
   description: "แปล move pokemon ภาษาไทย",
+  robots: "index , follow" ,
+  keywords: ["แปลท่าโปเกมอน" , "pokemon move แปลไทย" , "ชื่อท่าโปเกมอนภาษาไทย" , "ท่าโปเกมอนพร้อมคำแปล" , "รายชื่อท่าโปเกมอนภาษาไทย" , "ความหมายท่าโปเกมอน" , "รายชื่อท่าโจมตีโปเกมอนแปลไทย" , "แปลชื่อท่า pokemon"]
 };
 
 export default function RootLayout({
@@ -19,11 +23,29 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const structuredData = {
+    "@context":"https://schema.org",
+    "@type":"WebPage",
+    "name":"POKEMON MOVE THAI",
+    "description": "เว็บไซต์แปล move pokemon เป็นภาษาไทย พร้อมคำอธิบายความหมายและรายละเอียดท่าโจมตีต่างๆ",
+    "headline": "แปล move pokemon ภาษาไทย",
+    "url":"https://pokemonmovesthai.vercel.app/",
+    "inLanguage": "th-TH"
+  }
+  
   return (
-    <html lang="en">
+    <html lang="th">
+       <head>
+        <AdSense pId="8642023158448076"/>
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{ __html: JSON.stringify(structuredData) }}
+        />
+      </head>
       <body
         className={kanit.className}
       >
+        <Analytics/>
         {children}
       </body>
     </html>
